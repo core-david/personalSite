3abf99f2ee40a0393b101d102e510ea28246d617
core-david <davale1201@gmail.com>
2025-02-12 02:08:46 -0600

    titleChange

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,6 +1,6 @@
 module.exports = {
-  title: "Nulite starter for Eleventy",
-  shortTitle: "Nulite starter 💊",
+  title: "David Vargas",
+  shortTitle: "David Vargas",
   description:
     "A beautiful and elegant blog theme starter project por Eleventy",
   url: "https://example.com/",
